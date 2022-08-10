ae1a70a85968b77d505b1a489cc52f52e861287a
Fabri <frivera@folcode.com>
2022-08-10 17:59:55 -0500

    Se crea página de producto

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,7 +1,47 @@
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import axios from 'axios'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Typography from '@mui/material/Typography'
+import { CardActionArea } from '@mui/material'
+import styles from '../../styles/Product.module.css'
+
 const ProductDetails = () => {
+  const [product, setProduct] = useState({})
+  const router = useRouter()
+
+  useEffect(() => {
+    axios.get("https://fakestoreapi.com/products/"+router.query.id)
+      .then(res => {
+        console.log(res.data)
+        setProduct(res.data)
+      })
+      .catch(err => console.log(err))
+  }, [])
+
+
   return (
-    <div>
-      <h1>ProductDetails</h1>
+    <div className={styles.centrar}>
+      <Card sx={{ maxWidth: 700, height: 700 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="500"
+            image={product.image}
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {product.title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {product.description}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
     </div>
   )
 }
